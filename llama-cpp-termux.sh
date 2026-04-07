cdfe085c9ba2b4d41365a20186dcba1546b157d6
Romyx <157620445+RomyxR@users.noreply.github.com>
2026-04-07 20:57:23 +0300

    Update llama-cpp-termux.sh

--- llama-cpp-termux.sh
+++ llama-cpp-termux.sh
@@ -3,5 +3,7 @@ pkg install cmake git ninja clang
 
 git clone --depth 1 https://github.com/ggml-org/llama.cpp
 
+cd llama.cpp
+
 cmake -B build -G Ninja -DGGML_TESTS=OFF -DGGML_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j 6 --target llama-cli llama-server
